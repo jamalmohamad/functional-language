a70ab42201ad30378058b09b25f50432482a9a63
jamalmohamad <gitAccount@$1990>
2020-02-23 00:13:24 +0100

    Solve almost all questions

--- firstPractice.hs
+++ firstPractice.hs
@@ -22,3 +22,35 @@ even' x = x `mod` 2 == 0
 odd' :: Integer -> Bool
 odd' x = not(even' x) 
 
+
+-- Define a function which checks whether an integer divides another.
+-- 2`divides`4
+-- not (4`divides`2)
+-- 3`divides`9
+divides :: Integer -> Integer -> Bool
+divides x y = x `div` y == 0
+
+
+-- Define a function which calculates the area of a rectangle using two sides.
+-- area 6 10 == 60
+
+area :: Int -> Int -> Int
+area x y = x * y
+
+
+-- Check whether a triangle with three given sides can be drawn.
+-- triangleSides 2 1 2
+-- not (triangleSides 3 4 1)
+
+triangleSides :: Int -> Int -> Int -> Bool
+triangleSides x y z 
+    | x == z && y < x && y < z = True
+    | otherwise = False
+
+
+-- Check whether three integers are Pythagorean triples.
+-- pythagoreanTriple 3 4 5
+-- pythagoreanTriple 5 3 4
+-- not (pythagoreanTriple 2 3 4)
+pythagoreanTriple :: Int -> Int -> Int -> Bool
+pythagoreanTriple x y z = ((x^2) + (y^2)) == (z^2)
