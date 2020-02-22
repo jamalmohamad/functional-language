5ad843f4b16ba8af34292d9e15e84210f9ed433e
jamalmohamad <gitAccount@$1990>
2020-02-22 21:23:47 +0100

    fibonoci (not solved)

--- fifthPractice.hs
+++ fifthPractice.hs
@@ -8,3 +8,16 @@ fact :: Integer -> Integer
 fact 0 = 0
 fact 1 = 1
 fact x = x * fact (x-1)
+
+
+-- 2- Determine thenth Fibonacci number!
+-- fib 0 == 0
+-- fib 1 == 1
+-- fib 2 == 1
+-- fib 4 == 3
+-- fib 5 == 5
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib 2 = 1
