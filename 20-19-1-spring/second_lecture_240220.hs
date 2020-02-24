77f87dd65e8a6b1873187a42222f788dba764ded
MO <mohammed.alhayaj@ibm.com>
2020-02-24 09:01:32 +0100

    some notes from lecture two

---
+++ 20-19-1-spring/second_lecture_240220.hs
@@ -0,0 +1,18 @@
+-- Haskell
+-- expressions and declarations
+-- expressions
+-- 2 + 4 -> 6
+-----    --- normal form
+-- reducable
+-- expression
+-- (redex)
+
+
+--  -> (means evaluation, reduction, normalizating)
+
+-- Declaration
+-- triple x            = x*3
+          --             ----
+          -- argument    body
+
+-- function is a relation between input and output
