349ae031f99249e953c38c43cd5dda6a86d48c22
MO <mohammed.alhayaj@ibm.com>
2020-02-24 13:18:17 +0100

    missing solution for excersise 14

--- first_excersise.hs
+++ first_excersise.hs
@@ -54,3 +54,21 @@ triangleSides x y z
 -- not (pythagoreanTriple 2 3 4)
 pythagoreanTriple :: Int -> Int -> Int -> Bool
 pythagoreanTriple x y z = ((x^2) + (y^2)) == (z^2)
+
+
+
+-- Check whether a year is a leap year. 
+-- A year is considered a leap year if it can be divided by 4 but not 100.
+-- However,years divisible by 400 are also leap years:
+-- •1992, 1996, 2012, 2016 are leap years, as they are divisible by 4 but not 100.
+-- •1700, 1800, 1900 are not leaps, as they are divisible by 4 and also 100.
+-- •1600, 2000 are leap years, as they are divisible 100 but also 400. 
+-- isLeapYear 1992
+-- isLeapYear 1996
+-- isLeapYear 1600
+-- isLeapYear 2000
+-- not (isLeapYear 1700)
+-- not (isLeapYear 1800)
+
+
+-- missed solution --
