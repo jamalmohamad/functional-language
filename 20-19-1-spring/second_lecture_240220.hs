f077d4d58eabb9cdaedb02a016d9a2664f470924
MO <mohammed.alhayaj@ibm.com>
2020-02-24 10:15:00 +0100

    second lecture 24 feb 2020

--- 20-19-1-spring/second_lecture_240220.hs
+++ 20-19-1-spring/second_lecture_240220.hs
@@ -16,3 +16,17 @@
           -- argument    body
 
 -- function is a relation between input and output
+even' n = mod n 2 == 0
+
+-- odd' n = not (even' n)
+odd' n = mod n 2 /= 0
+
+-- order does not matter in haskell
+-- Exception: stackoverflow  memory will be filled off 
+-- :info (+)
+-- :info (*)
+
+-- mod 6 2+1
+-- mod 6 2 then +1 or means (mod 6 2) +1
+-- mod 6 (2+1)
+-- 6 `mod` 2 pow 4
