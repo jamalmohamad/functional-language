2ea1417967ee9147cbb3bc6a85f66756a4769ade
MO <mohammed.alhayaj@ibm.com>
2020-03-02 17:24:45 +0100

    define isLetter function and some function enhancments

--- 20-19-1-spring/Practice03.hs
+++ 20-19-1-spring/Practice03.hs
@@ -1,5 +1,6 @@
 module Practice03 where
 -- Pattern matching and guards
+import Data.Char
 
 not' :: Bool -> Bool
 not' True = False
@@ -21,9 +22,14 @@ or' True False  = True
 or' False True  = True
 or' False False = False
 
+or''' :: Bool -> Bool -> Bool
+or''' True _   = True
+or''' _ True  = True
+or''' _ _ = False
+
 or'' a b 
-    | a == True = True
-    | b == True = True
+    | a = True
+    | b  = True
     | otherwise = False
 
 
@@ -56,7 +62,11 @@ min' a b
     | a < b = a
     | otherwise = b
 
+isLetter' c = 'a' <= c && c <= 'z'
+
+
 isLetterOrSpace :: Char -> Bool
 isLetterOrSpace chr 
     | chr == ' ' = True
+    | isLetter' chr  = True
     | otherwise = False
