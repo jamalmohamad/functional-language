ea8b2c4d9234df1fc09ac2976d43481854ee7ab6
MO <mohammed.alhayaj@ibm.com>
2020-03-02 12:08:35 +0100

    stretch solved

--- Exercises/second_exercise.hs
+++ Exercises/second_exercise.hs
@@ -25,3 +25,26 @@ add (a1, b1) (a2, b2)
 
 mul :: Frac -> Frac -> Frac
 mul (a1, b1) (a2, b2) = ((a1 * a2), (b1 * b2))
+
+
+-- Calculate the remainder and quotient of dividing an integer with another.
+-- modDiv 10 5 == (0,2)
+-- modDiv 11 5 == (1,2)
+-- modDiv 7 7  == (0,1)
+-- modDiv 7 8  == (7,0)
+-- modDiv 7 1  == (0,7)
+modDiv :: Integer -> Integer -> Frac
+-- modDiv :: Integer -> Integer -> (Integer, Integer)
+modDiv x y = (x `mod` y, x `div` y)
+
+-- Solve quadratic equations. Lets assume the equations have two real solutions.
+-- For example, solutions of x2−6x+ 8:
+-- quadratic 1.0 (-6.0) 8.0 == (2.0, 4.0)
+
+
+
+-- Stretch a vector with a constant factor.
+-- stretch (2, 4) 2 == (4, 8)
+-- stretch (1, 6) 3 == (3, 18)
+stretch :: Frac -> Integer -> Frac
+stretch (x, y) a = (a*x, a*y) 
