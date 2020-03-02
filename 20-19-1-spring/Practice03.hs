2149efd279eee12ad815b63fca9bae38d234ac2e
MO <mohammed.alhayaj@ibm.com>
2020-03-02 16:46:57 +0100

    practice03 with some function definition exercise

---
+++ 20-19-1-spring/Practice03.hs
@@ -0,0 +1,47 @@
+module Practice03 where
+-- Pattern matching and guards
+
+not' :: Bool -> Bool
+not' True = False
+not' False = True
+
+isCubeEvenOrNegative :: Int -> Bool
+isCubeEvenOrNegative n
+    | even cn   = True
+    | cn < 0    = True
+    | otherwise = False
+    where
+        cn :: Int
+        cn = n * n * n
+
+-- define using pattern matching
+or' :: Bool -> Bool -> Bool
+or' True True   = True
+or' True False  = True
+or' False True  = True
+or' False False = False
+
+or'' a b 
+    | a == True = True
+    | b == True = True
+    | otherwise = False
+
+
+xor' :: (Bool, Bool) -> Bool
+xor' (x,y)
+    | x == False && y == False = False
+    | x == False && y == True  = True
+    | x == True && y == False = True
+    | otherwise = False 
+
+xor'' :: (Bool, Bool) -> Bool
+xor'' (False, False)   = False
+xor'' (False, True)   = True
+xor'' (True, False)   = True
+xor'' (True, True)   = False
+
+swap :: (Int, Int) -> (Int, Int)
+swap (x,y) = (y, x)
+
+swap3 :: (Int, (Int, Int)) -> ((Int, Int), Int)
+swap3 (a, (b, c)) = ((a, b), c)
