41146ff8257d4e8ef4e39ec9b20597822f77beb6
MO <mohammed.alhayaj@ibm.com>
2020-03-02 11:38:29 +0100

    distance and add solved

---
+++ Exercises/second_exercise.hs
@@ -0,0 +1,19 @@
+-- Define a function which measures the distance between two integers on the number line.
+-- distance 5 (-2) == 7
+-- distance (-2) 5 == 7
+-- distance 5 9    == 4
+-- Hint: in Haskell, the absolute value function is abs.
+distance :: Integer -> Integer -> Integer
+distance x y = abs(x - y)  
+
+-- Define the addition of fractions.
+-- add (1, 2) (1, 2) == (2, 2)
+-- add (4, 3) (6, 5) == (38, 15)
+type Frac =  (Integer, Integer)
+-- add :: (Integer -> Integer) -> (Integer -> Integer) -> (Integer -> Integer)
+add :: Frac -> Frac -> Frac
+add (a1, b1) (a2, b2)
+    | b1 /= b2 =  ((a1 * b2) + (a2 * b1), (b1 * b2)) 
+    | b1 == b2 = ((a1 + a2), (b2))
+
+-- what is case of united denomenator?
