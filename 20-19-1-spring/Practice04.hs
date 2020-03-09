abe518be433129b290a0f10a00b16c5f09950810
MO <mohammed.alhayaj@ibm.com>
2020-03-09 17:06:59 +0100

    having fun

---
+++ 20-19-1-spring/Practice04.hs
@@ -0,0 +1,52 @@
+--  f :: Int -> (Bool, Int) -> Int 
+--  f x (y , z) = if y then x + z else x - z
+
+
+--  When defining a function `f :: Bool -> Bool -> Bool -> Bool` 
+--  by giving all possible cases, how many cases are needed ?
+--   8 <- correct one
+--   6
+--   4
+--   2
+
+module Practice04 where
+
+-----------
+-- Lists --
+-----------
+
+l1 :: [Int]
+l1 = [1,2,3]
+
+l2 :: [Bool]
+l2 = [False, True]
+
+l3 :: [Char]
+l3 = "Hello" -- == 'H' : 'e' : 'l' : 'l' : 'o' : []
+
+headInt :: [Int] -> Int
+headInt (x : xs) = x
+headInt [] = error "headInt []"
+
+tailInt :: [Int] -> [Int]
+tailInt (x : xs) = xs
+
+isEmpty :: [Int] -> Bool
+isEmpty [] = True
+isEmpty _ = False
+
+-- length' :: [Int] -> Int
+length' xs = sum[1| _ <- xs]
+
+filter' :: (Int -> Bool) -> [Int] -> [Int]
+filter' f xs = [x | x <- xs, f x]
+ 
+countEven :: [Int] -> Int
+--countEven xs = length([n | n <- xs, even n])
+--countEven xs = length([n | n <- filter even xs])
+countEven xs = length(filter even xs)
+
+-- sumOfSuqares [x,y,...] == x*x + y*y + ...
+sumOfSquares :: [Int] -> Int
+sumOfSquares xs = sum[n * n | n <- xs] 
+
